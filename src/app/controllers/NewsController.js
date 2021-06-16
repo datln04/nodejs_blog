0e3193105affd66b1867fb02d641419c24f3fc40
Thanh Dat <datln0424@gmail.com>
2021-06-16 19:35:04 +0700

    Router_Controller

---
+++ src/app/controllers/NewsController.js
@@ -0,0 +1,15 @@
+
+
+class NewController{
+
+    //[Get] News
+    index(req,res){
+        res.render('news')
+    }  
+
+    show(req,res){
+        res.send('News Detail')
+    } 
+}
+
+module.exports = new NewController; 
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require("express"); // vao lock-json lay express ra
 const morgan = require("morgan");
 const handlebars  = require('express-handlebars');
 const path = require("path");
+const route = require('./routes');
 
 
 const app = express();  // return obj de xay dung website
@@ -26,22 +27,11 @@ app.set('view engine', '.hbs'); // set view engine la handlebars
 
 app.set('views', path.join(__dirname, 'resources\\views'));
 
+// routes init
+
+route(app);
 
 const port = 3000;
-
-app.get("/", (req,res) =>(res.render('home')));
-
-app.get("/news", (req,res) =>(res.render('news')));
-
-app.get("/search", (req,res) =>(res.render('search'))); // req.query
-
-app.post("/search", (req,res) => {
-
-    console.log(req.body)
-
-    res.send('');
-})
-
 
 app.listen(port, () => console.log(`Server is ready in port ${port}`));
 // route: lay root , response tra ve hello world. Lang nghe cong 3000 va in ra cau console in terminal
